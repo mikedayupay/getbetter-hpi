e585f62764228dfaef91bdf0f62e857af80032b7
Mike Dayupay <mike.dayupay03@gmail.com>
2015-07-11 14:28:52 +0800

    Added add patient and search menu to PatietListActivity. Added icons for add patient and search

--- app/src/main/java/com/dlsu/thesis/getbetter/PatientListActivity.java
+++ app/src/main/java/com/dlsu/thesis/getbetter/PatientListActivity.java
@@ -3,6 +3,8 @@ package com.dlsu.thesis.getbetter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 /**
@@ -82,5 +84,31 @@ public class PatientListActivity extends Activity
             detailIntent.putExtra(PatientDetailFragment.ARG_ITEM_ID, id);
             startActivity(detailIntent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if(id == R.id.action_add_patient) {
+            return true;
+        } else if(id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
