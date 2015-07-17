711a20ae2ef276e092875e4ac7b1034f0605dec9
Mike Dayupay <mike.dayupay03@gmail.com>
2015-07-17 23:35:44 +0800

    Layout for the patients ListView

---
+++ app/src/main/java/com/dlsu/thesis/getbetter/UserSessionManager.java
@@ -0,0 +1,7 @@
+package com.dlsu.thesis.getbetter;
+
+/**
+ * GetBetter. Created by Mike Dayupay on 7/17/15.
+ */
+public class UserSessionManager {
+}
---
+++ app/src/main/java/com/dlsu/thesis/getbetter/adapters/PatientsAdapters.java
@@ -0,0 +1,7 @@
+package com.dlsu.thesis.getbetter.adapters;
+
+/**
+ * GetBetter. Created by Mike Dayupay on 7/17/15.
+ */
+public class PatientsAdapters {
+}
