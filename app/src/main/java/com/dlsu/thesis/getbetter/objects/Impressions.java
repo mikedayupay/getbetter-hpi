28094a42582cd8175e4f0ddfed4e889e5836f76e
Mike Dayupay <mike.dayupay03@gmail.com>
2015-11-03 20:10:40 +0800

    added additional fields (scientific_name, local_name, etc) to Impressions class

--- app/src/main/java/com/dlsu/thesis/getbetter/objects/Impressions.java
+++ app/src/main/java/com/dlsu/thesis/getbetter/objects/Impressions.java
@@ -7,12 +7,26 @@ import java.util.ArrayList;
  */
 public class Impressions {
 
+    private int impressionId;
     private String impression;
+    private String scientificName;
+    private String localName;
+    private String treatmentProtocol;
+    private String remarks;
     private ArrayList<Symptom> symptoms;
 
-    public Impressions(String impression, ArrayList<Symptom> symptoms) {
+    public Impressions(int impressionId, String impression, String scientificName, String localName,
+                       String treatmentProtocol, String remarks) {
+        this.impressionId = impressionId;
         this.impression = impression;
-        this.symptoms = symptoms;
+        this.scientificName = scientificName;
+        this.localName = localName;
+        this.treatmentProtocol = treatmentProtocol;
+        this.remarks = remarks;
+    }
+
+    public int getImpressionId() {
+        return impressionId;
     }
 
     public String getImpression() {
@@ -29,5 +43,21 @@ public class Impressions {
 
     public void setSymptoms(ArrayList<Symptom> symptoms) {
         this.symptoms = symptoms;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public String getTreatmentProtocol() {
+        return treatmentProtocol;
+    }
+
+    public String getRemarks() {
+        return remarks;
     }
 }
