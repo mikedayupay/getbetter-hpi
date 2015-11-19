2dcbe3ab12041bf5b6e6310b647dc334eb2693f7
Mike Dayupay <mike.dayupay03@gmail.com>
2015-11-19 14:49:58 +0800

    Added expert system summary activity and positive results object class

---
+++ app/src/main/java/com/dlsu/thesis/getbetter/ExpertSystemSummaryActivity.java
@@ -0,0 +1,14 @@
+package com.dlsu.thesis.getbetter;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ExpertSystemSummaryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expert_system_summary);
+    }
+
+}
---
+++ app/src/main/java/com/dlsu/thesis/getbetter/objects/PositiveResults.java
@@ -0,0 +1,7 @@
+package com.dlsu.thesis.getbetter.objects;
+
+/**
+ * GetBetter. Created by Mike Dayupay on 11/19/15.
+ */
+public class PositiveResults {
+}
