03f64eb2fb7ef2988c3b91f4b77bb50c4da04659
Mike Dayupay <mike.dayupay03@gmail.com>
2015-11-24 18:06:31 +0800

    added PostivieResults object class

--- app/src/main/java/com/dlsu/thesis/getbetter/objects/PositiveResults.java
+++ app/src/main/java/com/dlsu/thesis/getbetter/objects/PositiveResults.java
@@ -4,4 +4,20 @@ package com.dlsu.thesis.getbetter.objects;
  * GetBetter. Created by Mike Dayupay on 11/19/15.
  */
 public class PositiveResults {
+
+    private String positiveName;
+    private String positiveAnswerPhrase;
+
+    public PositiveResults(String positiveName, String positiveAnswerPhrase) {
+        this.positiveName = positiveName;
+        this.positiveAnswerPhrase = positiveAnswerPhrase;
+    }
+
+    public String getPositiveName() {
+        return positiveName;
+    }
+
+    public String getPositiveAnswerPhrase() {
+        return positiveAnswerPhrase;
+    }
 }
